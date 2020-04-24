9bea242c19577057cd4746e7b5012e2bffee3906
Jose Daniel Hernandez Osorio <isc.danielosorio@hotmail.com>
2020-04-24 00:44:44 -0500

    integracion de primeros pasos

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+  listCourse: string[] = ['JAVA', 'PYTHON', 'C++', 'JAVASCRIPT', 'GO']
+
+  enable: boolean = true;
+
+  constructor() { }
+
+  setEnable(): void {
+    this.enable = (this.enable == true) ? false : true;
+   }
+
+}
